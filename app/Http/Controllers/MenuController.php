d370d08ca16308c699da6744f5ab51da7a7f6210
Hemahemabrsa1998 <ibrahim1998i@yahoo.com>
2024-01-26 18:31:51 +0300

    add qr menu code

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'menus' => Menu::all()
+        ]);
+    }
+
+    public function show($id)
+    {
+        $data = Menu::findOrFail($id);
+        return response()->json([
+            $data->meals
+        ]);
+    }
+
+    public function store()
+    {
+        $attributes = \request()->validate([
+            'name' => 'required|min:1|max:255'
+        ]);
+
+        Menu::create($attributes);
+
+        $data = [
+            'message'=>'Menu created',
+            'status'=>201
+        ];
+        return response()->json($data,201);
+
+    }
+}
---
+++ app/Models/Menu.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function meals()
+    {
+        return $this->hasMany(Meal::class);
+    }
+}
