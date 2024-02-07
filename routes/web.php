246a61b370643abb4512f2142cd04ba43d1940bf
Ibrahim Ismail <ibrahim1998i@yahoo.com>
2024-02-07 23:49:45 +0300

    setup the layout and routes for the login component

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\PostController;
 
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Livewire\Counter;
+use App\Livewire\Login;
 use App\services\Newsletter;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
@@ -22,7 +24,7 @@ Route::post('register',[RegisterController::class,'store'])->middleware('guest')
 
 Route::post('logout',[SessionsController::class,'destroy'])->middleware('auth');
 
-Route::get('login',[SessionsController::class,'create'])->middleware('guest');
+Route::get('login', Login::class)->middleware('guest');
 Route::post('sessions',[SessionsController::class,'store'])->middleware('guest');
 
 Route::get('admin/posts/create',[PostController::class,'create'])->middleware('can:admin');
@@ -30,3 +32,5 @@ Route::post('admin/posts',[PostController::class,'store'])->middleware('can:admi
 Route::get('admin/posts/{post}/edit',[PostController::class,'edit'])->middleware('can:admin');
 Route::patch('admin/posts/{post}',[PostController::class,'update'])->middleware('can:admin');
 Route::delete('admin/posts/{post}',[PostController::class,'destroy'])->middleware('can:admin');
+
+Route::get('/counter', Counter::class);
